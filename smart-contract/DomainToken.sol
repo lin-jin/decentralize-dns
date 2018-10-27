509cec47bcde4900a4dd318ebe46a8e3a06a5592
Lin Jin <aupdaisy@gmail.com>
2018-10-26 21:26:48 -0400

    Create DomainToken.sol

---
+++ smart-contract/DomainToken.sol
@@ -0,0 +1,62 @@
+pragma solidity ^0.4.25;
+
+import "./Token.sol";
+
+contract DomainToken is Token {
+    
+    string constant public name  = "DomainToken";
+    string constant public symbol = "DMT";
+    uint8 constant public decimals = 0;
+    
+    address contractOwner;
+    uint public totalDomainToken;
+    
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) approved;
+    
+    constructor () {
+        contractOwner = msg.sender;
+        totalDomainToken = 0;
+    }
+    
+    
+    function totalSupply() public view returns (uint256) {
+        return totalDomainToken;
+    }
+    
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
+    }
+    
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require (balances[msg.sender] >= _value && _value > 0);
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+    
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require (approved[_from][msg.sender] >= _value && balances[_from] >= _value && _value > 0);
+        balances[_from] -= _value;
+        balances[_to] += _value;
+        approved[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+    
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        approved[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+    
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        return approved[_owner][_spender];
+    }
+    
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    
+}

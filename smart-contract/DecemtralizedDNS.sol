b8a8ba19a9e2e39afaa7e277f8b598373b3471e9
Lin Jin <aupdaisy@gmail.com>
2018-10-27 22:12:17 -0400

    reward voters

--- smart-contract/DecemtralizedDNS.sol
+++ smart-contract/DecemtralizedDNS.sol
@@ -1,12 +1,16 @@
 pragma solidity ^0.4.25;
 
+import './DomainToken.sol';
+
 contract DecentralizedDNS {
     
+    address constant tokenContractAddr = 0xbbf289d846208c16edc8474705c748aff07732db;
+    DomainToken token = DomainToken(tokenContractAddr);
+    
     address contractOwner;
-    uint64 public numberOfDomains;
-    uint64 public constant updatePrice = 1 ether;
-    uint64 public constant maxRegTime = 5 minutes;
-    uint8 public validThrehold = 4; // verified weighted votes is at least 4 times unverified weighted votes
+    uint64 public numberOfClaimedDomains;
+    uint8 public validThrehold;
+    uint256 public tips;
     
     enum Stage {Init, Reg, Vote, Done}
     
@@ -22,6 +26,7 @@ contract DecentralizedDNS {
         string domain;
         string record;
         //uint expiration;
+        uint256 cost;
         uint time;
         Stage stage;
         mapping (address => CommitteeMember) committee;
@@ -38,7 +43,6 @@ contract DecentralizedDNS {
     mapping (bytes32 => Update) updatesTable;
     mapping (bytes32 => RecordSet) dnsRecordSets;
     
-    
     event NewUpdate (address proposer, string domain, bool needVote);
     event VoteResult (string domain, bool isValid);
     event RecordDeleted (string domain);
@@ -52,6 +56,9 @@ contract DecentralizedDNS {
     
     constructor () {
         contractOwner = msg.sender;
+        numberOfClaimedDomains = 0;
+        validThrehold = 4; // verified weighted votes is at least 4 times unverified weighted votes
+        tips = 0;
     }
     
     
@@ -62,8 +69,23 @@ contract DecentralizedDNS {
         //dnsRecordSets[_domainHash].expiration = updatesTable[_domainHash].expiration;
         dnsRecordSets[_domainHash].record = updatesTable[_domainHash].record;
         
+        numberOfClaimedDomains += 1;
+        
     }
     
+    
+    function deleteRecordSet (string _domain) public returns (bool) {
+        
+        require(msg.sender == dnsRecordSets[domainHash].owner);
+        
+        bytes32 domainHash = keccak256(_domain);
+        delete dnsRecordSets[domainHash].owner;
+        delete dnsRecordSets[domainHash].domain;
+        delete dnsRecordSets[domainHash].record;
+        numberOfClaimedDomains -= 1;
+        
+        emit RecordDeleted(_domain);
+    }
     
     function deleteUpdate (bytes32 _domainHash) private {
         
@@ -74,8 +96,31 @@ contract DecentralizedDNS {
         
     }
     
+    // msg.sender deposite tokens in the contract to claim the domain ownership
+    function deposite (uint256 _cost) private returns (bool) {
+        
+        require(token.allowance(msg.sender, this) >= _cost, "DMTs for update is not enough.");
+        token.transferFrom(msg.sender, this, _cost); //escrow transaction
+        return true;
+    }
     
-    function sendUpdate (string _domain, string _record) payable public {
+    // reward voters according to their voting weight
+    function rewardVoters (bytes32 _domainHash, address [] _voters, uint256 _totalWeightedVotes) private returns (bool) {
+        
+        uint256 rewards = updatesTable[_domainHash].cost;
+        uint256 tip = rewards;
+        for (uint i = 0; i < _voters.length; i++) {
+            uint256 reward = updatesTable[_domainHash].committee[_voters[i]].weight * rewards / _totalWeightedVotes;
+            token.transfer(_voters[i], reward);
+            tip -= rewards;
+        }
+        
+        tips += tip;
+        
+        return true;
+    }
+    
+    function sendUpdate (string _domain, string _record, uint256 _cost) public {
         
         bytes32 domainHash = keccak256(_domain);
         
@@ -84,13 +129,14 @@ contract DecentralizedDNS {
             emit NewUpdate (msg.sender, _domain, false);
         }
         else{
-            require (msg.value == updatePrice, "Price for update is not enough."); 
             require(updatesTable[domainHash].time == 0, "Domain update in progress.");
-            
+            require(deposite(_cost) == true);
+
             updatesTable[domainHash].proposer = msg.sender;
             updatesTable[domainHash].domain = _domain;
             updatesTable[domainHash].record = _record;
-            //updatesTable[domainHash].expiration = _expiration;
+            //updatesTable[domainHash].expiration = _expiration
+            updatesTable[domainHash].cost = _cost;
             updatesTable[domainHash].time = now;
             updatesTable[domainHash].stage = Stage.Reg;
             
@@ -106,7 +152,7 @@ contract DecentralizedDNS {
         require (updatesTable[domainHash].stage == Stage.Reg, "Not in registration stage.");
         require(updatesTable[domainHash].time != 0, "Domain update has not started.");
         require(updatesTable[domainHash].committee[msg.sender].isMember == false, "You are already a committee member for this domain update.");
-        require(now - updatesTable[domainHash].time <= maxRegTime, "Maximum registration time has reached.");
+        //require(now - updatesTable[domainHash].time <= maxRegTime, "Maximum registration time has reached.");
         
         updatesTable[domainHash].committeeMember.push(msg.sender);
         updatesTable[domainHash].committee[msg.sender].isMember = true;
@@ -125,7 +171,7 @@ contract DecentralizedDNS {
         
         updatesTable[domainHash].committee[msg.sender].voted = true;
         updatesTable[domainHash].committee[msg.sender].domainVerified = _domainVerified;
-        updatesTable[domainHash].committee[msg.sender].weight = msg.sender.balance;
+        updatesTable[domainHash].committee[msg.sender].weight = token.balanceOf(msg.sender);
         
         return true;
     }
@@ -133,19 +179,24 @@ contract DecentralizedDNS {
     
     function voteComplete (bytes32 _domainHash) private returns (bool) {
         
-        //require(updatesTable[domainHash].stage == Stage.Done, "Voting not yet finished.");
-        
         bool result;
         uint totalWeightedVerifiedVotes = 0;
         uint totalWeightedUnverifiedVotes = 0;
         
+        address[] verifiedVoters;
+        address[] unverifiedVoters;
+        
         for (uint i = 0; i < updatesTable[_domainHash].committeeMember.length; i++) {
-            if (updatesTable[_domainHash].committee[updatesTable[_domainHash].committeeMember[i]].isMember == true){
+            if (updatesTable[_domainHash].committee[updatesTable[_domainHash].committeeMember[i]].isMember == true) {
                 if (updatesTable[_domainHash].committee[updatesTable[_domainHash].committeeMember[i]].voted == true) {
+                    if (updatesTable[_domainHash].committee[updatesTable[_domainHash].committeeMember[i]].domainVerified == true) {
                     totalWeightedVerifiedVotes += updatesTable[_domainHash].committee[updatesTable[_domainHash].committeeMember[i]].weight;
-                }
-                else {
-                    totalWeightedUnverifiedVotes += updatesTable[_domainHash].committee[updatesTable[_domainHash].committeeMember[i]].weight;
+                    verifiedVoters.push(updatesTable[_domainHash].committeeMember[i]);
+                    }
+                    else {
+                        totalWeightedUnverifiedVotes += updatesTable[_domainHash].committee[updatesTable[_domainHash].committeeMember[i]].weight;
+                        unverifiedVoters.push(updatesTable[_domainHash].committeeMember[i]);
+                    }
                 }
             }
         }
@@ -153,9 +204,11 @@ contract DecentralizedDNS {
         if (totalWeightedVerifiedVotes / validThrehold >= totalWeightedUnverifiedVotes) {
             result = true;
             addRecordSet(_domainHash);
+            rewardVoters(_domainHash, verifiedVoters, totalWeightedVerifiedVotes);
         }
         else {
             result = false;
+            rewardVoters(_domainHash, unverifiedVoters, totalWeightedUnverifiedVotes);
         }
         
         deleteUpdate(_domainHash);
@@ -163,18 +216,6 @@ contract DecentralizedDNS {
         emit VoteResult(updatesTable[_domainHash].domain, result);
     }
     
-    
-    function deleteRecord (string _domain) public returns (bool) {
-        
-        require(msg.sender == dnsRecordSets[domainHash].owner);
-        
-        bytes32 domainHash = keccak256(_domain);
-        delete dnsRecordSets[domainHash].owner;
-        delete dnsRecordSets[domainHash].domain;
-        delete dnsRecordSets[domainHash].record;
-        
-        emit RecordDeleted(_domain);
-    }
     
     
     function getUpdate (string _domain) public view returns (address, string, string, uint, Stage, address[]) {

cdb5d5f6f5af7b7f3d467dd9c61c0361d15ba690
Lin Jin <aupdaisy@gmail.com>
2018-11-11 22:18:19 -0500

    ganache-cli only accepts lower case on events, while web3.py must use checksum addr

--- application/ddns.py
+++ application/ddns.py
@@ -1,4 +1,4 @@
-from web3 import Web3, HTTPProvider, TestRPCProvider
+from web3 import Web3, HTTPProvider
 import dns.resolver
 import json
 import sys
@@ -97,28 +97,49 @@ def deduct_domaintokens(num):
 
 web3 = Web3(HTTPProvider("http://127.0.0.1:8545"))
 
-account = sys.argv[1]
-if not account in web3.eth.accounts:
+account = sys.argv[1].lower()
+if not account in (account.lower() for account in web3.eth.accounts):
 	sys.exit()
 web3.eth.defaultAccount = web3.toChecksumAddress(account)
 
-token_contract_addr = web3.toChecksumAddress('0x95befd09e2af01a17ffac5ae6ab71b22a1cd31a3')
+token_contract_addr = web3.toChecksumAddress('0xe0888415ff17a88d88a3b92b34bff93403a61bd5')
 with open('token_contract_abi.json', 'r') as file:
 	token_contract_abi = file.read()
 
-ddns_contract_addr = web3.toChecksumAddress('0x1c5b7b794a1b419e557ea59475667a3e5684d384')
+ddns_contract_addr = web3.toChecksumAddress('0xd532e9e78fb2ab4d34521da99d46b449612cc197')
 with open('ddns_contract_abi.json', 'r') as file:
 	ddns_contract_abi = file.read()
 
 token_contract = web3.eth.contract(token_contract_addr, abi = token_contract_abi)
 ddns_contract = web3.eth.contract(ddns_contract_addr, abi = ddns_contract_abi)
 
-to = web3.toChecksumAddress('0xee4dfee3f626b72c8acb16302ed1d525456951a7')
+to = web3.toChecksumAddress('0xd532e9e78fb2ab4d34521da99d46b449612cc197')
 value = 2
 tx_hash = transfer(to, value)
+
 tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
 logs = token_contract.events.Transfer().processReceipt(tx_receipt)
 
 print(logs)
+
+
+
+def handle_event(event):
+    print(event)
+
+
+def log_loop(event_filter, poll_interval):
+    while True:
+        for event in event_filter.get_new_entries():
+            handle_event(event)
+        time.sleep(poll_interval)
+
+def main():
+    block_filter = w3.eth.filter('latest')
+    log_loop(block_filter, 2)
+
+
+
+ event_filter = token_contract.events.Transfer.createFilter(fromBlock = "latest")
 
 
